f0527910e70a57f1a2737569d3671c6b2fae8032
LyubomiRadev <lyubomiradev@gmail.com>
2023-10-31 21:12:27 +0200

    Style fix for oxyPlot.

--- AvaloniaApplication1/Views/MainWindow.axaml.cs
+++ AvaloniaApplication1/Views/MainWindow.axaml.cs
@@ -12,20 +12,20 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
 
-		var plotModel = new PlotModel { Title = "World population by continent" };
-		var pieSeries = new PieSeries() { TextColor = OxyColors.BlueViolet };
-		pieSeries.Slices.Add(new PieSlice("Africa", 1030) { IsExploded = true });
-		pieSeries.Slices.Add(new PieSlice("Americas", 929) { IsExploded = true });
-		pieSeries.Slices.Add(new PieSlice("Asia", 4157));
-		pieSeries.Slices.Add(new PieSlice("Europe", 739) { IsExploded = true });
-		pieSeries.Slices.Add(new PieSlice("Oceania", 35) { IsExploded = true });
-		pieSeries.InnerDiameter = 0.2;
-		pieSeries.ExplodedDistance = 0;
-		pieSeries.Stroke = OxyColors.Black;
-		pieSeries.StrokeThickness = 1.0;
-		pieSeries.AngleSpan = 360;
-		pieSeries.StartAngle = 0;
-		plotModel.Series.Add(pieSeries);
+		//var plotModel = new PlotModel { Title = "World population by continent" };
+		//var pieSeries = new PieSeries() { TextColor = OxyColors.BlueViolet };
+		//pieSeries.Slices.Add(new PieSlice("Africa", 1030) { IsExploded = true, Fill = OxyColors.AliceBlue });
+		//pieSeries.Slices.Add(new PieSlice("Americas", 929) { IsExploded = true, Fill = OxyColors.Beige });
+		//pieSeries.Slices.Add(new PieSlice("Asia", 4157));
+		//pieSeries.Slices.Add(new PieSlice("Europe", 739) { IsExploded = true, Fill = OxyColors.Red });
+		//pieSeries.Slices.Add(new PieSlice("Oceania", 35) { IsExploded = true, Fill = OxyColors.Green });
+		//pieSeries.InnerDiameter = 0.2;
+		//pieSeries.ExplodedDistance = 0;
+		//pieSeries.Stroke = OxyColors.Black;
+		//pieSeries.StrokeThickness = 1.0;
+		//pieSeries.AngleSpan = 360;
+		//pieSeries.StartAngle = 0;
+		//plotModel.Series.Add(pieSeries);
 
 		Continents = new ObservableCollection<ContinentItem>
 		{
@@ -36,7 +36,7 @@ public partial class MainWindow : Window
 			new ContinentItem { Name = "Oceania", PopulationInMillions = 35, IsExploded = true }
 		};
 
-		this.PieModel = plotModel;
+		//this.PieModel = plotModel;
 		this.DataContext = new { PieModel, Continents };
 	}
 
